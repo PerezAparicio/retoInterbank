04564641d776124a350860ffb3c9bb52a660a501
Ricardo Perez <ricardo.aparicio97@gmail.com>
2025-04-29 04:03:05 -0500

    Agregando comentarios javadoc y eliminando comnetarios

--- src/main/java/com/rperez/retointerbank/GenerateReport.java
+++ src/main/java/com/rperez/retointerbank/GenerateReport.java
@@ -10,6 +10,8 @@ import java.util.Objects;
 import java.util.Optional;
 
 /**
+ * Clase encargada de generar reportes de transacciones en consola a partir de un archivo CSV.
+ *
  * @author Ricardo Perez
  */
 @Component
@@ -36,39 +38,20 @@ public class GenerateReport {
      * @throws Exception Si ocurre un error durante la lectura del archivo CSV.
      */
     public void generateReport() throws Exception {
-        // 1- Lectura de archivos csv y almacenarlos en una lista
-        //      del mismo tipo que se pasa como argumento a readCsv en este caso "Transaction"
+        
         List<Transaction> transactions = readCsv.readCsv(Transaction.class);
 
-        // 2.- De la lista obtenida, hago uso del ApiStream de Java para filtrar por los metodos:
-        // filter: El atributo type igualandolo al valor del ENUM.CREDIT que es la constante de "Crédito"
-        // map: Transforma el resultado al tipo de dato que tiene una variable que se pasa como argumento
-        // reduce: Acumula los resultados de la lista segun como se especifique, en este caso, use la funcion
-        //          BigDecimal::add para sumar los datos iniciando el contador por el 0
         BigDecimal finalBalance = transactions.stream().filter(x -> Objects.equals(x.getType(), TypeTransaction.CREDIT.getType())).map(Transaction::getAmount).reduce(BigDecimal.ZERO, BigDecimal::add);
 
-        // 3.- De la lista obtenida en el punto 1 utilice el ApiStream de Java usando el metodo
-        // max: Se utiliza para obtener el valor maximo de una lista, pasando como argumento el atributo
-        //      amount dentro de un Comparartor.comparing.
-        // Comparartor.comparing: El método ordena los valores que se le pasa como argumento de forma ascendente.
         Optional<Transaction> maxAmountTransaction = transactions.stream().max(Comparator.comparing(Transaction::getAmount));
 
-        // 4.- De la lista obtenida en el paso 1 utilice el ApiStream de Java usando los métodos:
-        //  filter: Igualando el atributo type con el valor del ENUM.CREDIT que es la constante de "Crédito"
-        //  count: Hace una cuenta del número de elementos que tiene la lista.
         long maxTransactionCredito = transactions.stream().filter(x -> Objects.equals(x.getType(), TypeTransaction.CREDIT.getType())).count();
-        // 5.- De la lista obtenida en el paso 1 utilice el ApiStream de Java usando los métodos:
-        //  filter: Igualando el atributo type con el valor del ENUM.DEBIT que es la constante de "Débito"
-        //  count: Hace una cuenta del número de elementos que tiene la lista.
+
         long maxTransactionDebito = transactions.stream().filter(x -> Objects.equals(x.getType(), TypeTransaction.DEBIT.getType())).count();
 
-        // 6.- Finalmente imprimo los valores en System.out.println
         System.out.println("Reporte de transacciones");
         System.out.println("--------------------------------------------------");
         System.out.println("Balance Final: " + finalBalance);
-
-        // 7.- Utilice una operación ternaria para trabajar con el Optional que obtuve en el paso 3
-        //      si el optional tiene valor imprime el resultado si no imprimirá 0
         System.out.println("Transacción de Mayor Monto: " + (maxAmountTransaction.isPresent() ? "ID " + maxAmountTransaction.get().getId() + " - " + maxAmountTransaction.get().getAmount() : 0));
         System.out.println("Conteo de Transacciones: Crédito: " + maxTransactionCredito + " Débito: " + maxTransactionDebito);
     }
--- src/main/java/com/rperez/retointerbank/ReadCsv.java
+++ src/main/java/com/rperez/retointerbank/ReadCsv.java
@@ -14,6 +14,8 @@ import java.util.List;
 import java.util.Objects;
 
 /**
+ * Clase encargada de leer y procesar archivos CSV.
+ *
  * @author Ricardo Perez
  */
 @Component
@@ -44,63 +46,37 @@ public class ReadCsv {
      */
     public <T> List<T> readCsv(Class<T> model) throws Exception {
 
-        // 1.- Se instancia el BeanListProcessor de Tipo model
-        // BeanListProcessor: Sirve para convertir datos a objetos almacenandolos en una lista.
-        // El objeto lo determina en este caso el argumento model.
         BeanListProcessor<T> processor = new BeanListProcessor<>(model);
 
-        // 2.- Se instancia el CsvParserSettings
-        // CsvParserSettings: Sirve para configurar el comportamiento del componente CsvParser,
-        // controla como se lee el archivo CSV y como se procesan los datos.
         CsvParserSettings settings = new CsvParserSettings();
 
-        // Activa la lectura de la primera fila del archivo y lo toma como encabezado y así mapear
-        // las columnas con la del objeto
         settings.setHeaderExtractionEnabled(true);
-
-        // Aqui es donde se enlaza el BeanListProcessor para que pueda almacenarse los datos procesados.
         settings.setProcessor(processor);
-
-        // Habilita la deteccion automatica del separador de lineas
         settings.setLineSeparatorDetectionEnabled(true);
 
-        // 3.- Obtengo la dirección de mi ruta donde van a estar mis archivos csv
         File filesCsv = new File(pathCsv);
 
-        // 4.- Valido si la ruta existe o no
         if (!filesCsv.exists()) {
             return Collections.emptyList();
         }
 
-        // 5.- Listo los archivos dentro de la ruta con el cuyos nombre de archivos terminen con la extensión .csv
         File[] files = filesCsv.listFiles((dir, name) -> name.endsWith(".csv"));
 
-        // 6.- Valido si files es nulo ó si la cantidad de elementos que contiene es 0
         if (Objects.isNull(files) || files.length == 0) {
             return Collections.emptyList();
         }
 
-        // 7.- Instancio una lista vacia de tipo Genérica.
         List<T> allResult = new ArrayList<>();
 
-        //8.- Instancio la clase CsvParser: se utiliza para leer y analizar los datos de un archivo csv
-        //      usando una configuracion que se determino en pasos anteriores.
         CsvParser parser = new CsvParser(settings);
 
-        //9.- Iterar los archivos dentro de mi ruta
         for (File csv : files) {
-            //10.- Leer el archivo
             try (FileReader fileReader = new FileReader(csv)) {
-                //11.- Parsear el contenido del archivo csv a la de la variable de BeanListProcessor
                 parser.parse(fileReader);
             }
-            //12.- El resultado del parseo de datos se almacena en la variable allResult
-            //      Esto para ir almacenando los datos de cada archivo ya que la librería
-            //      por cada iteracion libera el espacio en el BeanListProcessor
             allResult.addAll(processor.getBeans());
         }
 
-        // 13.- Devuelvo todos los resultados de mi lectura a los arhcivos .csv
         return allResult;
     }
 
